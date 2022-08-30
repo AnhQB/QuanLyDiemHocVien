a9b526a38a55eeae50034b3695e08b5622e97777
anhqb <101201273+AnhQB@users.noreply.github.com>
2022-08-30 08:49:40 +0700

    update validate import_studentgroup

--- app/Imports/StudentGroupImport.php
+++ app/Imports/StudentGroupImport.php
@@ -3,7 +3,10 @@
 namespace App\Imports;
 
 use App\Models\Group;
+use App\Models\MajorSubject;
+use App\Models\Student;
 use App\Models\StudentGroup;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Maatwebsite\Excel\Concerns\ToArray;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -16,21 +19,48 @@ class StudentGroupImport implements ToArray, WithHeadingRow
     {
 
         $arr = explode('_', $this->filename);
+        $degree_id = $arr[0];
+        $major_id = $arr[1];
+        $group_id = $arr[2];
+
+        $subject_id = $arr[3];
+        $major_subject = MajorSubject::query()
+                    ->where([
+                        ['major_id', $major_id],
+                        ['subject_id', $subject_id],
+                    ])
+                    ->firstOrFail();
+        if(!$major_subject){
+            throw new \RuntimeException("Môn theo chuyên ngành chưa được khởi tạo");
+        }
+
         if(Group::query()
             ->where([
-                ['degree_id', $arr[0]],
-                ['major_id', $arr[1]],
-                ['id', $arr[2]],
-                ['subject_id', $arr[3]],
+                ['degree_id', $degree_id],
+                ['major_id', $major_id],
+                ['id', $group_id],
+                ['subject_id', $subject_id],
             ])
             ->doesntExist()
         ){
             throw new \RuntimeException("Lớp không tồn tại với tên file");
         }
         foreach ($array as $each){
-            $group_id = $arr[2];
-            $subject_id = $arr[3];
+
             $student_id = $each['id'];
+            try {
+                Student::query()
+                    ->where([
+                        ['id', $student_id],
+                        ['degree_id', $degree_id],
+                        ['major_id', $major_id],
+                        ['semester_major', $major_subject->semester_major],
+                    ])
+                    ->firstOrFail();
+            }catch (ModelNotFoundException $e){
+                throw new \RuntimeException("học sinh có mã không thuộc chuyên ngành hoặc khoá này");
+            }
+
             StudentGroup::query()
                 ->updateOrCreate([
                     'group_id' => $group_id,
